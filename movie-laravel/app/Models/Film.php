4b76aaa3392eb4830aad1d277be7d701fc078e62
zyxkoo15 <ykkhmrdn@gmail.com>
2022-12-19 20:58:07 +0700

    kbbmi

---
+++ movie-laravel/app/Models/Film.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Film extends Model
+{
+    use HasFactory;
+
+    protected $table = 'film';
+    protected $fillable = [
+        'judul',
+        'sinopsis',
+        'tahun',
+        'rating',
+        'img_url',
+    ];
+}
